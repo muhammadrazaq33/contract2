6d1c6223a6f52b7371d785fc859b601f45ef4493
muhammadrazaq33 <muhammadrazaq5913483@gmail.com>
2024-08-06 22:29:46 -0700

    transaction Price section completed

--- src/components/Transactions/TransactionPriceSection.jsx
+++ src/components/Transactions/TransactionPriceSection.jsx
@@ -1,22 +1,21 @@
-import React from 'react'
+import React from "react";
 import navIcon from "../../../public/assets/algorand-algo-icon.svg";
 import transaction2 from "../assets1/transaction-2.svg";
 import transaction3 from "../assets1/transaction-3.svg";
 import transaction4 from "../assets1/transaction-4.svg";
 
-// md:border-l border-[#e9ecef]
 const TransactionPriceSection = () => {
   return (
     <div className=" ">
       <div
-        className="card py-10  mb-4 mt-[-3rem] rounded-[0.75rem] bg-[#121212]"
+        className="card md:py-10 py-6  mb-4 mt-[-3rem] rounded-[0.75rem] bg-[#121212]"
         style={{ boxShadow: "0 0.5rem 1.2rem rgb(189 197 209 / 20%)" }}
       >
-        <div className="grid grid-cols-12 gap-4 md:gap-0">
+        <div className="grid grid-cols-12 gap-4 md:gap-0 lg:px-0 md:px-0 px-2">
           {/* first block */}
-          <div className="md:col-span-6 lg:col-span-3 md:px-[2.5rem] col-span-12 px-[15px]">
+          <div className="md:col-span-6 lg:col-span-3 md:px-[2.5rem] col-span-12 px-[15px] lg:py-0 py-4 md:border-b-0 border-b border-[#23262F]">
             <div className="">
-              <div className="flex-grow">
+              <div className="flex-grow lg:mb-0 mb-2">
                 <a
                   class="text-[#FFF] text-[20px] font-medium"
                   href="#"
@@ -38,9 +37,9 @@ const TransactionPriceSection = () => {
           </div>
           {/* end of first block */}
           {/* second block */}
-          <div className="md:col-span-6 lg:col-span-3 md:px-[2.5rem] col-span-12 px-[15px] md:border-l border-[#e9ecef]">
+          <div className="md:col-span-6 lg:col-span-3 md:px-[2.5rem] col-span-12 px-[15px] md:border-l border-[#23262F] lg:py-0 py-4 md:border-b-0 border-b">
             <div className="">
-              <div className="flex-grow">
+              <div className="flex-grow lg:mb-0 mb-2">
                 <a
                   class="text-[#FFF] text-[20px] font-medium"
                   href="#"
@@ -59,9 +58,9 @@ const TransactionPriceSection = () => {
           </div>
           {/* end of second block */}
           {/* third section */}
-          <div className="md:col-span-6 lg:col-span-3 md:px-[2.5rem] col-span-12 px-[15px] md:border-l border-[#e9ecef]">
+          <div className="md:col-span-6 lg:col-span-3 md:px-[2.5rem]  col-span-12 px-[15px] lg:border-l border-spacing-x-72 border-[#23262F] lg:border-t-0 md:border-t lg:py-0 py-4 md:border-b-0 border-b">
             <div className="">
-              <div className="flex-grow">
+              <div className="flex-grow lg:mb-0 mb-2">
                 <a
                   class="text-[#FFF] text-[20px] font-medium"
                   href="#"
@@ -83,9 +82,9 @@ const TransactionPriceSection = () => {
           </div>
           {/* end of third section */}
           {/* fourth section */}
-          <div className="md:col-span-6 lg:col-span-3 md:px-[2.5rem] col-span-12 px-[15px] md:border-l border-[#e9ecef]">
+          <div className="md:col-span-6 lg:col-span-3 md:px-[2.5rem] col-span-12 px-[15px] md:border-l border-[#23262F] lg:py-0 py-4 lg:border-t-0 md:border-t">
             <div className="">
-              <div className="flex-grow">
+              <div className="flex-grow lg:mb-0 mb-2">
                 <a
                   class="text-[#FFF] text-[20px] font-medium"
                   href="#"
@@ -110,6 +109,6 @@ const TransactionPriceSection = () => {
       </div>
     </div>
   );
-}
+};
 
-export default TransactionPriceSection
+export default TransactionPriceSection;
